51b3cfe6655ea6ee8bbbe83c5b0f2804438e1429
Bego-GarciaConde <91980267+Bego-GarciaConde@users.noreply.github.com>
2023-02-20 16:14:31 +0100

    coment a line

--- ED_PS_fitting.py
+++ ED_PS_fitting.py
@@ -3,7 +3,7 @@
 
 from os.path import expanduser
 
-home = expanduser("~")
+# home = expanduser("~")
 # from skimage import feature
 from sklearn import linear_model, datasets
 from sklearn.linear_model import RANSACRegressor
